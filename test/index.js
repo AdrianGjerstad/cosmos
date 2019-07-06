4b4e4d151ca45b75f6bd8ce67d37b1f0b1c2779a
Adrian Gjerstad <adgjerstad@gmail.com>
2019-07-06 17:27:10 +0000

    Added PRNGs

--- test/index.js
+++ test/index.js
@@ -1,9 +1,7 @@
 try {
-  let mf = new Cosmos.MathFunction(function(x) {
-    return x*x;
-  });
-
-  alert(Cm.integral(mf, 0, 1, 0.0000001).integral);
+  let seeder = Cosmos.PRNG.xmur3("this is a test of the xmur3 mixer");
+  let prng = Cosmos.PRNG.sfc32(seeder(), seeder(), seeder(), seeder());
+  alert(prng() + " " + prng() + " " + prng() + " " + prng());
 } catch(e) {
   alert("Oops! An error occured!\n" + e.name + ": " + e.message);
 }
