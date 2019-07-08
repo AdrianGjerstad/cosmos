a416fa95262ae9c569cde261bdcd98730921d69f
Adrian Gjerstad <adgjerstad@gmail.com>
2019-07-08 15:51:11 +0000

    Added Dictionary

--- test/index.js
+++ test/index.js
@@ -1,7 +1,14 @@
 try {
-  let seeder = Cosmos.PRNG.xmur3("this is a test of the xmur3 mixer");
-  let prng = Cosmos.PRNG.sfc32(seeder(), seeder(), seeder(), seeder());
-  alert(prng() + " " + prng() + " " + prng() + " " + prng());
+  let dict = Cm.Dictionary.loadJSON(
+    {length_:2,en_US:{length_:2,lang_:0,hello:"Hello",bye:"Bye"},es_MX:{length_:2,lang_:1,hello:"Hola",bye:"Adios"}}
+  );
+
+  dict.setLanguage("en_US");
+
+  alert(dict.read("hello") + " " + dict.read("bye"));
+  dict.setLanguage("es_MX");
+  alert(dict.read("hello") + " " + dict.read("bye"));
+  alert(dict.languageCount() + " " + dict.keyCount());
 } catch(e) {
   alert("Oops! An error occured!\n" + e.name + ": " + e.message);
 }
