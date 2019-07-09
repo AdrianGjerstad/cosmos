5ed0972bc1d0c2b9f90aabc514c0818138b235f2
Adrian Gjerstad <adgjerstad@gmail.com>
2019-07-09 23:16:38 +0000

    Added AlertBox

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 try {
-  let index = 0;
+  /*let index = 0;
   let ticking_engine = new Cm.TickingEngine(function() {
     index++;
   }, 60);
@@ -7,6 +7,9 @@ try {
 
   setTimeout(function() {
     alert("Called " + index + " times");
+  }, 1000);*/
+  setTimeout(function() {
+    Cm.alert("foo", "bar");
   }, 1000);
 } catch(e) {
   alert("Oops! An error occured!\n" + e.name + ": " + e.message);
