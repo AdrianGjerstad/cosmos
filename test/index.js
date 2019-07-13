44f052200c05b7f85d36d434f6893212a92465ce
Adrian Gjerstad <adgjerstad@gmail.com>
2019-07-13 18:56:37 +0000

    Added image fix, transformations, and fonts

--- test/index.js
+++ test/index.js
@@ -1,19 +1,21 @@
 try {
-  let testImageSrc = "https://www.w3schools.com/jsref/img_the_scream.jpg";
+  let testImageSrc = "../cosmos.png";
   let testImage;
-  Cm.mediaLoader = function() {
-    testImage = Cm.loadImage(testImageSrc);
+  Cosmos.mediaLoader = function() {
+    testImage = Cosmos.loadImage(testImageSrc);
   };
-  Cm.ready = function() {
-    let engine = new Cm.GameEngine(
+  Cosmos.ready = function() {
+    let engine = new Cosmos.GameEngine(
     function(d) {
-      Cm.alert("Test", JSON.stringify(testImage));
+
     },
     function(r, d) {
       r.background(200);
+      r.translate(r.w/2, r.h/2);
+      r.rotate(Math.PI/4);
       r.image(testImage, 0, 0);
     },
-    60, 220, 277);
+    60, 512, 512);
     engine.r.border();
     engine.start();
   };
