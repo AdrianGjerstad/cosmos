3dd04db3c77840e284020aaa6b63f91eee0aeb00
Adrian Gjerstad <adgjerstad@gmail.com>
2019-07-13 20:45:08 +0000

    Fixed errors

--- test/index.js
+++ test/index.js
@@ -1,24 +1,21 @@
-try {
-  let testImageSrc = "../cosmos.png";
-  let testImage;
-  Cosmos.mediaLoader = function() {
-    testImage = Cosmos.loadImage(testImageSrc);
-  };
-  Cosmos.ready = function() {
-    let engine = new Cosmos.GameEngine(
-    function(d) {
+let testImageSrc = "../cosmos.png";
+let testImage;
+Cosmos.mediaLoader = function() {
+  testImage = Cosmos.loadImage(testImageSrc);
+  Cm.alertOfError(()=>{throw new Error("foo");});
+};
+Cosmos.ready = function() {
+  let engine = new Cosmos.GameEngine(
+  function(d) {
 
-    },
-    function(r, d) {
-      r.background(200);
-      r.translate(r.w, r.h);
-      r.rotate(Math.PI);
-      r.image(testImage, 0, 0);
-    },
-    60, 512, 512);
-    engine.r.border();
-    engine.start();
-  };
-} catch(e) {
-  alert("Oops! An error occured!\n" + e.name + ": " + e.message);
-}
+  },
+  function(r, d) {
+    r.background(200);
+    r.translate(r.w, r.h);
+    r.rotate(Math.PI);
+    r.image(testImage, 0, 0);
+  },
+  60, 512, 512);
+  engine.r.border();
+  engine.start();
+};
